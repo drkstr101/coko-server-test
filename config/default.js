4a79f08cb7c6a7bd97bd8119e1ec892bc98aeeba
Aaron Miller <amiller@aps.org>
2023-10-03 16:17:25 +0000

    fix: syntax unsupported in ES5

--- config/default.js
+++ config/default.js
@@ -1,10 +1,10 @@
 module.exports = {
   "pubsweet-server": {
     db: {
-      user: process.env["POSTGRES_USER"] ?? "postgres",
-      host: process.env["POSTGRES_HOST"] ?? "localhost",
-      database: process.env["POSTGRES_DB"] ?? "test",
-      password: process.env["POSTGRES_PASSWORD"] ?? "postgres",
+      user: process.env["POSTGRES_USER"] || "postgres",
+      host: process.env["POSTGRES_HOST"] || "localhost",
+      database: process.env["POSTGRES_DB"] || "test",
+      password: process.env["POSTGRES_PASSWORD"] || "postgres",
       port: parseInt(process.env["POSTGRES_PORT"], 10) || 5432,
     },
   },
