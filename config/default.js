abde6d3542b3119cb4863f76f26ce2e683471360
Aaron Miller <amiller@aps.org>
2023-10-03 16:14:11 +0000

    add db-manager test

---
+++ config/default.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pubsweet-server": {
+    db: {
+      user: process.env["POSTGRES_USER"] ?? "postgres",
+      host: process.env["POSTGRES_HOST"] ?? "localhost",
+      database: process.env["POSTGRES_DB"] ?? "test",
+      password: process.env["POSTGRES_PASSWORD"] ?? "postgres",
+      port: parseInt(process.env["POSTGRES_PORT"], 10) || 5432,
+    },
+  },
+  dbManager: {
+    username: "admin",
+    password: "password",
+    email: "admin@example.com",
+    admin: true,
+    clobber: true,
+  },
+};
---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const { Collection, User } = require("@pubsweet/models");
+const { setupDb } = require("@pubsweet/db-manager");
+const config = require("config");
+
+const seed = async () => {
+  console.log("Loaded Config:", config.util.toObject());
+
+  await setupDb({
+    username: "admin",
+    password: "password",
+    email: "admin@example.com",
+    admin: true,
+    clobber: true,
+  });
+
+  const user = await new User({
+    username: "john",
+    email: "john@example.com",
+    password: "johnjohn",
+  }).save();
+
+  const collection = new Collection({
+    title: "My Blog",
+    owners: [user.id],
+  });
+
+  await collection.save();
+
+  logger.info("Seeding complete.");
+  // process.exit(1);
+};
+
+seed();
